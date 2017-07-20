59d72171152d09c73a6d6d19a2db09a57a475962
Kees de Wit <keesdewit@keesdewit.nl>
2017-07-20 09:52:34 +0200

    Update amounts

--- src/main.cpp
+++ src/main.cpp
@@ -1603,7 +1603,7 @@ double ConvertBitsToDouble(unsigned int nBits)
     }
 
     return dDiff;
-}
+} 
 
 int64_t GetBlockValue(int nHeight)
 {
@@ -1615,7 +1615,7 @@ int64_t GetBlockValue(int nHeight)
     }
 
     if (nHeight == 0) {
-        nSubsidy = 120000001 * COIN;
+        nSubsidy = 80000001 * COIN;
     } else if (nHeight < 86400 && nHeight > 0) {
         nSubsidy = 250 * COIN;
     } else if (nHeight < 151200 && nHeight >= 86400) {
